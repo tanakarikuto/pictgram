fcfa9d12d897df7919ce040f20fc1cefc6c250c7
tanakarikuto <rikuto616tanaka@gmail.com>
2021-10-09 09:46:44 +0000

    pictgram

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  root 'pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+root 'pages#index'
+get 'pages/help'
+end
+
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/link'
+end
