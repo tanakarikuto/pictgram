889e27697dad78169dea6d4c56fd0fb654e6e079
tanakarikuto <rikuto616tanaka@gmail.com>
2021-12-06 13:21:47 +0000

    rails04 validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 15}}
+  
+  
   validates :email, {presence: true}
+  #「~@~.~」のバリデーション
+  validates :email, format: { with:  URI::MailTo::EMAIL_REGEXP}
+  
+  
   validates :password, :password_confirmation, length: {minimum: 8, maximum: 32}
-  
-   #アルファベット、数字の混合のみ可能
-  #validates :password, :password_confirmation, inclusion: {in: [a-z/d]}
-  
-  #メールアドレスの正規表現　「~ @ ~ . ~」
-  #validates :email, format {with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i}
-  
+  #英数混合
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+   
   has_secure_password
   
   has_many :topics
